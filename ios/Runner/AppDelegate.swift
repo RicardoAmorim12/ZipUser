5505791c559c1cd0f985e059186ee33794c4aab5
Ricardo Paiva Amorim <43219598+RicardoAmorim12@users.noreply.github.com>
2024-11-02 18:15:59 -0300

    API MAPS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("AIzaSyCaCSJ0BZItSyXqBv8vpD1N4WBffJeKhLQ")
+    GMSServices.provideAPIKey("AIzaSyAwNxmaPoZ94IqxTBD9bZRLXN3tAm-O3J4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
